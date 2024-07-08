582af60f119d9c907a52f8f05f99803b3925c799
markymark5127 <markmayne@ymail.com>
2024-07-08 10:26:06 -0400

    size and routing
    fixed sizing and routing foor the webpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
+import { LoginComponent } from './login/login.component';
+import { NotificationComponent } from './notification/notification.component';
+import { PostComponent } from './post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'feed', component: FeedComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'notification', component: NotificationComponent },
+  { path: 'post', component: PostComponent },
+  { path: '', redirectTo: '/feed', pathMatch: 'full' },  // Redirect to feed by default
+  { path: '**', redirectTo: '/feed' }  // Wildcard route for a 404 page or redirect
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
